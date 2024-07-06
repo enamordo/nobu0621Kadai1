4c53c295aa257edaf3354b70a77564720e6151c7
Atsunobu Yuge <yugechan666@gmail.com>
2024-07-06 18:02:11 +0900

    Initial Commit

---
+++ Kadai1/Kadai1App.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai1App.swift
+//  Kadai1
+//
+//  Created by 弓削　敦信 on 2024/07/06.
+//
+
+import SwiftUI
+
+@main
+struct Kadai1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
