06aeb87f23699697bd5a718c3bfc0affcd2b3548
Atsunobu Yuge <yugechan666@gmail.com>
2024-07-08 20:36:00 +0900

    変数、プロジェクトファイル等の修正　

--- Kadai1/ContentView.swift
+++ Kadai1/ContentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var inputNumber1:String  = ""
-    @State private var inputNumber2:String  = ""
-    @State private var inputNumber3:String  = ""
-    @State private var inputNumber4:String  = ""
-    @State private var inputNumber5:String  = ""
-    @State private var totalNumber: Int = 0
+    @State private var inputNumber1: String  = ""
+    @State private var inputNumber2: String  = ""
+    @State private var inputNumber3: String  = ""
+    @State private var inputNumber4: String  = ""
+    @State private var inputNumber5: String  = ""
+    @State private var totalNumber: String = "Label"
 
     var body: some View {
         VStack {
@@ -30,7 +30,7 @@ struct ContentView: View {
             }
             HStack {
                 // Labelにはタイトルだけ表示
-                Label(String(totalNumber), systemImage: "bolt.fill")
+                Label(totalNumber, systemImage: "bolt.fill")
                     .labelStyle(.titleOnly)
                 Spacer()
             }
@@ -43,8 +43,8 @@ struct ContentView: View {
 /* 下記エラーになるため、メソッド化して処理を分割:
  The compiler is unable to type-check this expression in reasonable time; try breaking up the expression into distinct sub-expression
  */
-func add(number1: Int, number2: Int, number3: Int, number4: Int, number5: Int) -> Int {
-    return number1 + number2 + number3 + number4 + number5
+func add(number1: Int, number2: Int, number3: Int, number4: Int, number5: Int) -> String {
+    return String(number1 + number2 + number3 + number4 + number5)
 }
 
 #Preview {
